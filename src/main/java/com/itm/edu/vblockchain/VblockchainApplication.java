c5ab9f51f52050f1cee6129470a18a87f59fd157
Jonathan G - Mac <Jonathan@Mac-JGzzL.local>
2018-09-04 21:55:35 -0500

    initial commit

---
+++ src/main/java/com/itm/edu/vblockchain/VblockchainApplication.java
@@ -0,0 +1,12 @@
+package com.itm.edu.vblockchain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VblockchainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VblockchainApplication.class, args);
+	}
+}
